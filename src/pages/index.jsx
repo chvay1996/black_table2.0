71cbec249f63b9a4e24162b34ad350e71917d814
chvay1996 <105976963+chvay1996@users.noreply.github.com>
2024-02-17 04:42:31 +0200

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,7 +6,6 @@ const Home = () => {
       <h1 style={{ color: "white" }}>
         Приветствую вас на страницах нашей таблицы! 
       </h1>
-<h1> Maxzirka с фп напиши плиз в ТГ @Sllipick </h1>
       <h1 style={{ color: "white" }}>Выберите сервер!</h1>
     </div>
   );
